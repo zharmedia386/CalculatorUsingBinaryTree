2b39e96e615ebfc96474a06c03d513d7a9165fbc
zharmedia386 <muhammadazharalauddin@gmail.com>
2021-08-04 16:29:57 +0700

    ShowColorTheme.c

--- applicationUI.c
+++ applicationUI.c
@@ -263,5 +263,79 @@ void credit(){
     printf("\n\n\n  Press BACKSPACE on your keyboard to main menu");
 }
 
+void showColorTheme(char kodeWarna[]){
+	//Black
+ 		if(kodeWarna[0] == '0' && kodeWarna[1] == 'A') system("color 0A");
+ 		if(kodeWarna[0] == '0' && kodeWarna[1] == 'B') system("color 0B");
+ 		if(kodeWarna[0] == '0' && kodeWarna[1] == 'C') system("color 0C");
+ 		if(kodeWarna[0] == '0' && kodeWarna[1] == 'D') system("color 0D");
+ 		if(kodeWarna[0] == '0' && kodeWarna[1] == 'E') system("color 0E");
+ 		if(kodeWarna[0] == '0' && kodeWarna[1] == 'F') system("color 0F");
+ 	//Blue
+ 		if(kodeWarna[0] == '1' && kodeWarna[1] == 'A') system("color 1A");
+ 		if(kodeWarna[0] == '1' && kodeWarna[1] == 'B') system("color 1B");
+ 		if(kodeWarna[0] == '1' && kodeWarna[1] == 'C') system("color 1C");
+ 		if(kodeWarna[0] == '1' && kodeWarna[1] == 'D') system("color 1D");
+ 		if(kodeWarna[0] == '1' && kodeWarna[1] == 'E') system("color 1E");
+ 		if(kodeWarna[0] == '1' && kodeWarna[1] == 'F') system("color 1F");
+ 	//Green
+ 		if(kodeWarna[0] == '2' && kodeWarna[1] == 'A') system("color 2A");
+ 		if(kodeWarna[0] == '2' && kodeWarna[1] == 'B') system("color 2B");
+ 		if(kodeWarna[0] == '2' && kodeWarna[1] == 'C') system("color 2C");
+ 		if(kodeWarna[0] == '2' && kodeWarna[1] == 'D') system("color 2D");
+ 		if(kodeWarna[0] == '2' && kodeWarna[1] == 'E') system("color 2E");
+ 		if(kodeWarna[0] == '2' && kodeWarna[1] == 'F') system("color 2F");
+	//Aqua
+		if(kodeWarna[0] == '3' && kodeWarna[1] == 'A') system("color 3A");
+ 		if(kodeWarna[0] == '3' && kodeWarna[1] == 'B') system("color 3B");
+ 		if(kodeWarna[0] == '3' && kodeWarna[1] == 'C') system("color 3C");
+ 		if(kodeWarna[0] == '3' && kodeWarna[1] == 'D') system("color 3D");
+ 		if(kodeWarna[0] == '3' && kodeWarna[1] == 'E') system("color 3E");
+ 		if(kodeWarna[0] == '3' && kodeWarna[1] == 'F') system("color 3F");
+	//Red
+		if(kodeWarna[0] == '4' && kodeWarna[1] == 'A') system("color 4A");
+ 		if(kodeWarna[0] == '4' && kodeWarna[1] == 'B') system("color 4B");
+ 		if(kodeWarna[0] == '4' && kodeWarna[1] == 'C') system("color 4C");
+ 		if(kodeWarna[0] == '4' && kodeWarna[1] == 'D') system("color 4D");
+ 		if(kodeWarna[0] == '4' && kodeWarna[1] == 'E') system("color 4E");
+ 		if(kodeWarna[0] == '4' && kodeWarna[1] == 'F') system("color 4F");
+	//Purple
+		if(kodeWarna[0] == '5' && kodeWarna[1] == 'A') system("color 5A");
+ 		if(kodeWarna[0] == '5' && kodeWarna[1] == 'B') system("color 5B");
+ 		if(kodeWarna[0] == '5' && kodeWarna[1] == 'C') system("color 5C");
+ 		if(kodeWarna[0] == '5' && kodeWarna[1] == 'D') system("color 5D");
+ 		if(kodeWarna[0] == '5' && kodeWarna[1] == 'E') system("color 5E");
+ 		if(kodeWarna[0] == '5' && kodeWarna[1] == 'F') system("color 5F");
+	//Yellow
+		if(kodeWarna[0] == '6' && kodeWarna[1] == 'A') system("color 6A");
+ 		if(kodeWarna[0] == '6' && kodeWarna[1] == 'B') system("color 6B");
+ 		if(kodeWarna[0] == '6' && kodeWarna[1] == 'C') system("color 6C");
+ 		if(kodeWarna[0] == '6' && kodeWarna[1] == 'D') system("color 6D");
+ 		if(kodeWarna[0] == '6' && kodeWarna[1] == 'E') system("color 6E");
+ 		if(kodeWarna[0] == '6' && kodeWarna[1] == 'F') system("color 6F");
+	//White
+		if(kodeWarna[0] == '7' && kodeWarna[1] == 'A') system("color 7A");
+ 		if(kodeWarna[0] == '7' && kodeWarna[1] == 'B') system("color 7B");
+ 		if(kodeWarna[0] == '7' && kodeWarna[1] == 'C') system("color 7C");
+ 		if(kodeWarna[0] == '7' && kodeWarna[1] == 'D') system("color 7D");
+ 		if(kodeWarna[0] == '7' && kodeWarna[1] == 'E') system("color 7E");
+ 		if(kodeWarna[0] == '7' && kodeWarna[1] == 'F') system("color 7F");
+	//Grey
+		if(kodeWarna[0] == '8' && kodeWarna[1] == 'A') system("color 8A");
+ 		if(kodeWarna[0] == '8' && kodeWarna[1] == 'B') system("color 8B");
+ 		if(kodeWarna[0] == '8' && kodeWarna[1] == 'C') system("color 8C");
+ 		if(kodeWarna[0] == '8' && kodeWarna[1] == 'D') system("color 8D");
+ 		if(kodeWarna[0] == '8' && kodeWarna[1] == 'E') system("color 8E");
+ 		if(kodeWarna[0] == '8' && kodeWarna[1] == 'F') system("color 8F");
+	//Light Blue
+		if(kodeWarna[0] == '9' && kodeWarna[1] == 'A') system("color 9A");
+ 		if(kodeWarna[0] == '9' && kodeWarna[1] == 'B') system("color 9B");
+ 		if(kodeWarna[0] == '9' && kodeWarna[1] == 'C') system("color 9C");
+ 		if(kodeWarna[0] == '9' && kodeWarna[1] == 'D') system("color 9D");
+ 		if(kodeWarna[0] == '9' && kodeWarna[1] == 'E') system("color 9E");
+ 		if(kodeWarna[0] == '9' && kodeWarna[1] == 'F') system("color 9F");
 
+	// Keluar
+		printf("\n\n\n  Press ENTER on your keyboard to main menu");
+ }
 #endif
